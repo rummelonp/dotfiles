8228492b446fb44eea747b76a9e7791b43752734
Kazuya Takeshima <mail@mitukiii.jp>
2013-01-07 21:37:31 +0900

    Refactor env.

--- .zsh/lib/misc.zsh
+++ .zsh/lib/misc.zsh
@@ -14,23 +14,21 @@ case $OSTYPE in
 esac
 
 # Path
-export PATH=$HOME/bin:/usr/local/bin:$PATH
+export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH
 export RSENSE_HOME=$HOME/.emacs.d/el-get/rsense
 
 case $OSTYPE in
     darwin*)
-        # Rbenv
+        # Ruby
         eval "$(rbenv init -)"
         export RBENV_ROOT=/usr/local/opt/rbenv
         # Perlbrew
         export PERLBREW_ROOT=/usr/local/perlbrew
         source $PERLBREW_ROOT/etc/bashrc
         # Node
-        export NODE_PATH=/usr/local/lib/node_modules
+        export NODE_PATH=/usr/local/share/npm/lib/node_modules
         # Android
         export ANDROID_HOME=/usr/local/var/lib/android-sdk
-        export PATH=$PATH:$ANDROID_HOME/tools
-        export PATH=$PATH:$ANDROID_HOME/platform-tools
         # Gisty
         export GISTY_DIR=$HOME/Dropbox/Gist
         ;;
