2f7e1d2a1cd40547b2e71b7978b566a7a6f965af
Kazuya Takeshima <mail@mitukiii.jp>
2013-01-09 03:57:04 +0900

    Add rurema.

--- .emacs.d/init.d/language-ruby.el
+++ .emacs.d/init.d/language-ruby.el
@@ -49,6 +49,8 @@
 (add-hook 'ruby-mode-hook
           '(lambda ()
              (require 'rsense)
+             (setq rsense-rurema-home (expand-file-name "~/Dropbox/Documents/ruby-refm"))
+             (setq rsense-rurema-refe "refe-1_9_3")
              (add-to-list 'ac-sources 'ac-source-rsense-method)
              (add-to-list 'ac-sources 'ac-source-rsense-constant)
              (define-key ruby-mode-map (kbd "C-x .") 'ac-complete-rsense)))
