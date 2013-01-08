1f4a045e1b51b13611a09b3877b0452caa20ccd8
Kazuya Takeshima <mail@mitukiii.jp>
2013-01-09 02:59:54 +0900

    Fix el-get install.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,12 +1,14 @@
 ;; el-get
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-(unless (require 'el-get nil t)
-  (url-retrieve
-   "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
-   (lambda (s)
-     (let (el-get-master-branch)
-       (goto-char (point-max))
-       (eval-print-last-sexp)))))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (let (el-get-master-branch)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (el-get 'sync 'package)
+  (el-get-elpa-build-local-recipes))
 (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get/recipes")
 (el-get 'sync)
 
