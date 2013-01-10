c1eb32f88ee03c37ab741aeb3eea963f67bf5580
Kazuya Takeshima <mail@mitukiii.jp>
2013-01-11 02:20:44 +0900

    Fix migemo dict path at .emacs.d

--- .emacs.d/init.d/extensions.el
+++ .emacs.d/init.d/extensions.el
@@ -54,7 +54,7 @@
 (setq migemo-regex-dictionary nil)
 (setq migemo-coding-system 'utf-8-unix)
 (cond (darwin-p
-       (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict"))
+       (setq migemo-dictionary "/usr/local/Cellar/cmigemo/20110227/share/migemo/utf-8/migemo-dict"))
       (linux-p
        (setq migemo-dictionary "/usr/share/cmigemo/utf-8/migemo-dict")))
 (migemo-init)
