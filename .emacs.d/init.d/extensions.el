013473eb26d8b1e575a2d6252e319ec5c000b3be
Kazuya Takeshima <mail@mitukiii.jp>
2013-02-22 13:17:06 +0900

    Set cmigemo fullpath.

--- .emacs.d/init.d/extensions.el
+++ .emacs.d/init.d/extensions.el
@@ -51,7 +51,7 @@
 
 ;; migemo
 (require 'migemo)
-(setq migemo-command "cmigemo")
+(setq migemo-command "/usr/local/bin/cmigemo")
 (setq migemo-options '("-q" "--emacs"))
 (setq migemo-user-dictionary nil)
 (setq migemo-regex-dictionary nil)
