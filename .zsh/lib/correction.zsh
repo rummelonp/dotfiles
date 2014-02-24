f509f26e1350a08827cd43352d21a77ed914e983
Kazuya Takeshima <mail@mitukiii.jp>
2014-02-24 15:53:55 +0900

    Nocorrect git

--- .zsh/lib/correction.zsh
+++ .zsh/lib/correction.zsh
@@ -4,6 +4,7 @@ setopt correct_all
 
 alias cp="nocorrect cp"
 alias exec="nocorrect exec"
+alias git="nocorrect git"
 alias mkdir="nocorrect mkdir"
 alias mv="nocorrect mv"
 alias which="nocorrect which"
