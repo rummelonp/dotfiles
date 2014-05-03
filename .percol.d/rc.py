42a80ebf3804b6e8c1b8d16fedd933ebac513f03
Kazuya Takeshima <mail@mitukiii.jp>
2014-05-04 08:32:45 +0900

    Refactor percol configuration

--- .percol.d/rc.py
+++ .percol.d/rc.py
@@ -3,58 +3,58 @@ from percol.command import SelectorCommand
 from percol.key import SPECIAL_KEYS
 from percol.finder import FinderMultiQueryString, FinderMultiQueryRegex
 
-## prompt
+## Prompt
 def dynamic_prompt():
-    prompt = ur""
+    prompt = ur''
     if percol.model.finder.__class__ == FinderMultiQueryRegex:
-        prompt += "[Regexp]"
+        prompt += '[Regexp]'
     else:
-        prompt += "[String]"
+        prompt += '[String]'
     if percol.model.finder.case_insensitive:
-        prompt += "[a]"
+        prompt += '[a]'
     else:
-        prompt += "[A]"
-    prompt += "> %q"
+        prompt += '[A]'
+    prompt += '> %q'
     return prompt
 
 percol.view.__class__.PROMPT = property(lambda self: dynamic_prompt())
 
-## kill
-if sys.platform == "darwin":
+## Kill
+if sys.platform == 'darwin':
     def copy_end_of_line_as_kill(self):
-        commands.getoutput("echo " + self.model.query[self.model.caret:] + " | pbcopy")
+        commands.getoutput('echo ' + self.model.query[self.model.caret:] + ' | pbcopy')
         self.model.query  = self.model.query[:self.model.caret]
 
     def paste_as_yank(self):
-        self.model.insert_string(commands.getoutput("pbpaste"))
+        self.model.insert_string(commands.getoutput('pbpaste'))
 
     SelectorCommand.kill_end_of_line = copy_end_of_line_as_kill
     SelectorCommand.yank = paste_as_yank
 
-## keymap
-# map backspace
+## Keymap
+# Map backspace
 SPECIAL_KEYS.update({
     127: '<backspace>'
 })
 percol.import_keymap({
-    "C-a" : lambda percol: percol.command.beginning_of_line(),
-    "C-e" : lambda percol: percol.command.end_of_line(),
-    "C-b" : lambda percol: percol.command.backward_char(),
-    "C-f" : lambda percol: percol.command.forward_char(),
-    "C-d" : lambda percol: percol.command.delete_forward_char(),
-    "C-h" : lambda percol: percol.command.delete_backward_char(),
-    "C-k" : lambda percol: percol.command.kill_end_of_line(),
-    "C-y" : lambda percol: percol.command.yank(),
-    "C-n" : lambda percol: percol.command.select_next(),
-    "C-p" : lambda percol: percol.command.select_previous(),
-    "C-v" : lambda percol: percol.command.select_next_page(),
-    "M-v" : lambda percol: percol.command.select_previous_page(),
-    "M-<" : lambda percol: percol.command.select_top(),
-    "M->" : lambda percol: percol.command.select_bottom(),
-    "C-m" : lambda percol: percol.finish(),
-    "C-j" : lambda percol: percol.finish(),
-    "C-g" : lambda percol: percol.cancel(),
-    "M-c" : lambda percol: percol.command.toggle_case_sensitive(),
-    "M-q" : lambda percol: percol.command.toggle_finder(FinderMultiQueryString),
-    "M-r" : lambda percol: percol.command.toggle_finder(FinderMultiQueryRegex)
+    'C-a' : lambda percol: percol.command.beginning_of_line(),
+    'C-e' : lambda percol: percol.command.end_of_line(),
+    'C-b' : lambda percol: percol.command.backward_char(),
+    'C-f' : lambda percol: percol.command.forward_char(),
+    'C-d' : lambda percol: percol.command.delete_forward_char(),
+    'C-h' : lambda percol: percol.command.delete_backward_char(),
+    'C-k' : lambda percol: percol.command.kill_end_of_line(),
+    'C-y' : lambda percol: percol.command.yank(),
+    'C-n' : lambda percol: percol.command.select_next(),
+    'C-p' : lambda percol: percol.command.select_previous(),
+    'C-v' : lambda percol: percol.command.select_next_page(),
+    'M-v' : lambda percol: percol.command.select_previous_page(),
+    'M-<' : lambda percol: percol.command.select_top(),
+    'M->' : lambda percol: percol.command.select_bottom(),
+    'C-m' : lambda percol: percol.finish(),
+    'C-j' : lambda percol: percol.finish(),
+    'C-g' : lambda percol: percol.cancel(),
+    'M-c' : lambda percol: percol.command.toggle_case_sensitive(),
+    'M-q' : lambda percol: percol.command.toggle_finder(FinderMultiQueryString),
+    'M-r' : lambda percol: percol.command.toggle_finder(FinderMultiQueryRegex)
 })
