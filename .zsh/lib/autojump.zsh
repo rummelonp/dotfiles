020f1015e5dd42a0592f556cb866e68f83332456
Kazuya Takeshima <mail@mitukiii.jp>
2015-02-04 17:02:26 +0900

    Update autojump configuration

--- .zsh/lib/autojump.zsh
+++ .zsh/lib/autojump.zsh
@@ -2,6 +2,6 @@
 
 if [ -f /usr/share/autojump/autojump.zsh ]; then
     source /usr/share/autojump/autojump.zsh
-elif [ $commands[brew] -a -f $(brew --prefix)/etc/autojump.zsh ]; then
-    source $(brew --prefix)/etc/autojump.zsh
+elif [[ -f $(brew --prefix)/etc/profile.d/autojump.sh ]]; then
+    source $(brew --prefix)/etc/profile.d/autojump.sh
 fi
