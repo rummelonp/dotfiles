508db4feb938d050b17c56060863331e8440c715
Kazuya Takeshima <mail@mitukiii.jp>
2015-10-16 15:22:50 +0900

    Configure php-mode

--- .emacs.d/init.d/language-php.el
+++ .emacs.d/init.d/language-php.el
@@ -3,9 +3,3 @@
 ;;; php mode
 (autoload 'php-mode "php-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
-(add-hook 'php-mode-hook
-          '(lambda()
-             ;; configuration
-             (setq tab-width 2)
-             (setq indent-tabs-mode t)
-             (setq c-basic-offset 2)))
