05bbafb9959083a67786c67b62343770646c41e8
Kazuya Takeshima <mail@mitukiii.jp>
2015-10-16 19:48:38 +0900

    Configure php-mode

--- .emacs.d/init.d/language-php.el
+++ .emacs.d/init.d/language-php.el
@@ -3,3 +3,12 @@
 ;;; php mode
 (autoload 'php-mode "php-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+(add-to-list 'auto-mode-alist '("\\/views?.*\\.php$" . html-mode))
+
+;; toggle major mode between html and php
+(add-hook 'php-mode-hook
+          '(lambda ()
+             (define-key php-mode-map (kbd "C--") 'html-mode)))
+(add-hook 'html-mode-hook
+          '(lambda ()
+             (define-key html-mode-map (kbd "C--") 'php-mode)))
