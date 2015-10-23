c82c38f7219e513799358aeb771b3d0b82a826b6
Kazuya Takeshima <mail@mitukiii.jp>
2015-10-23 21:02:40 +0900

    Refactor setup script

---
+++ setup.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+DOTFILES_HOME=$(cd $(dirname $0) && pwd)
+DOTFILES=(
+    # zsh
+    .zshenv
+    .zshrc
+    .zsh
+    # bin
+    bin
+    # emacs
+    .emacs.d
+    # git
+    .gitconfig
+    .gitignore
+    # tmux
+    .tmux.conf
+    .tmux
+    # ruby
+    .gemrc
+    .pryrc
+    .railsrc
+    # python
+    .percol.d
+)
+
+for name in ${DOTFILES[@]}; do
+    [ ! -e $HOME/$name ] &&
+        ln -sv $DOTFILES_HOME/$name $HOME/$name
+done
+
+if [[ $OSTYPE =~ darwin ]]; then
+    [ ! -e $HOME/.tmux.local ] &&
+        ln -sv $HOME/.tmux/darwin.conf $HOME/.tmux.local
+else
+    [ ! -e $HOME/.tmux.local ] &&
+        ln -sv $HOME/.tmux/linux.conf $HOME/.tmux.local
+fi
+
+touch $HOME/.zshrc.local
