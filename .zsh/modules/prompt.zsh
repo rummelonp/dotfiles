d80ea15a57b4e48c49791bac1690dc47c10317d2
Kazuya Takeshima <mail@mitukiii.jp>
2017-03-23 17:06:10 +0900

    Show version of python and node to rprompt

--- .zsh/modules/prompt.zsh
+++ .zsh/modules/prompt.zsh
@@ -16,23 +16,37 @@ zstyle ':vcs_info:git:*' actionformats '(%s) - [%c%u%b|%a]'
 autoload -Uz add-zsh-hook
 
 function _precmd-rprompt() {
-    psvar=()
     LANG=en_US.UTF-8 vcs_info
     psvar[1]=$vcs_info_msg_0_
+}
+add-zsh-hook precmd _precmd-rprompt
+
+function _chpwd-rprompt() {
     if which rbenv > /dev/null; then
         psvar[2]=$(rbenv version | cut -d' ' -f1)
     fi
+    if which pyenv > /dev/null; then
+        psvar[3]=$(pyenv version | cut -d' ' -f1)
+    fi
+    if which nodebrew > /dev/null; then
+        psvar[4]=$(nodebrew ls | tail -n1 | cut -d ' ' -f2)
+    fi
 }
-add-zsh-hook precmd _precmd-rprompt
+add-zsh-hook chpwd _chpwd-rprompt
+
+psvar=()
+_chpwd-rprompt
 
 ## Prompt
 autoload -Uz colors
 colors
 
 reset="%{${reset_color}%}"
-green="%{${fg[green]}%}"
 red="%{${fg[red]}%}"
+green="%{${fg[green]}%}"
 blue="%{${fg[blue]}%}"
+magenta="%{${fg[magenta]}%}"
+cyan="%{${fg[cyan]}%}"
 white="%{${fg[white]}%}"
 
 if [ -n "$SSH_TTY" -o "$USER" = 'root' ]; then
@@ -40,7 +54,7 @@ if [ -n "$SSH_TTY" -o "$USER" = 'root' ]; then
 else
     PROMPT="%(?|${green}（╹ω╹）|${red}（☓ω☓）)${blue}%~${reset}$ "
 fi
-RPROMPT="%1(v|${green}%1v${reset} |)%2(v|${red}(%2v)${reset} |)"
+RPROMPT="%1(v|${green}%1v${reset} |)%2(v|${red}(%2v)${reset} |)%3(v|${magenta}(%3v)${reset} |)%4(v|${cyan}(%4v)${reset} |)"
 SPROMPT='Correct "%R" to "%r" [n,y,a,e]? ' # no, yes, abort, edit
 
 unset reset
