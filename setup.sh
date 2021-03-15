aac0c80ef919974d030b1cd03b3b4b55fe21470f
Kazuya Takeshima <mail@mitukiii.jp>
2021-03-15 17:02:58 +0900

    Change directory structure

--- setup.sh
+++ setup.sh
@@ -1,43 +1,56 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 DOTFILES_HOME=$(cd $(dirname $0) && pwd)
-DOTFILES=(
-    # zsh
-    .zsh
-    .zshenv
-    .zprofile
-    .zshrc
-    # bin
-    bin
-    # emacs
-    .emacs.d
-    # vim
-    .vimrc
-    # git
-    .gitconfig
-    .gitignore
-    # tmux
-    .tmux.conf
-    .tmux
-    # ruby
-    .gemrc
-    .pryrc
-    .railsrc
-    # python
-    .percol.d
-)
 
-for name in ${DOTFILES[@]}; do
-    [ ! -e $HOME/$name ] &&
-        ln -sv $DOTFILES_HOME/$name $HOME/$name
-done
+function link_file() {
+    local src=$DOTFILES_HOME/$1 dest=$HOME/$2
+    ln -fsv $src $dest
+}
 
-if [[ $OSTYPE =~ darwin ]]; then
-    [ ! -e $HOME/.tmux.local ] &&
-        ln -sv $HOME/.tmux/darwin.conf $HOME/.tmux.local
-else
-    [ ! -e $HOME/.tmux.local ] &&
-        ln -sv $HOME/.tmux/linux.conf $HOME/.tmux.local
-fi
+function link_dir() {
+    local src=$DOTFILES_HOME/$1 dest=$HOME/$2
+    if [ -L $dest ]; then rm $dest; fi
+    ln -sv $src $dest
+}
+
+# homebrew
+link_file homebrew/.Brewfile .Brewfile
+
+# zsh
+link_dir  zsh/.zsh      .zsh
+link_file zsh/.zshenv   .zshenv
+link_file zsh/.zprofile .zprofile
+link_file zsh/.zshrc    .zshrc
 
 touch $HOME/.zshrc.local
+
+# emacs
+link_dir emacs .emacs.d
+
+# vim
+link_file vim/.vimrc .vimrc
+
+# git
+link_file git/.gitconfig .gitconfig
+link_file git/.gitignore .gitignore
+
+# tmux
+link_dir  tmux/.tmux      .tmux
+link_file tmux/.tmux.conf .tmux.conf
+
+if [[ $OSTYPE =~ darwin ]]; then
+    link_file .tmux/darwin.conf .tmux.local
+else
+    link_file .tmux/linux.conf  .tmux.local
+fi
+
+# ruby
+link_file ruby/.gemrc   .gemrc
+link_file ruby/.pryrc   .pryrc
+link_file ruby/.railsrc .railsrc
+
+# percol
+link_dir percol .percol.d
+
+# bin
+link_dir bin .bin
