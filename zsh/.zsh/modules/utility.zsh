8baf75eff6d768d6a7e422893c23096456272bd8
Kazuya Takeshima <mail@mitukiii.jp>
2021-07-02 07:02:49 +0900

    Change directory color of ls

--- zsh/.zsh/modules/utility.zsh
+++ zsh/.zsh/modules/utility.zsh
@@ -16,8 +16,8 @@ alias which='nocorrect which'
 ### Aliases ###
 
 # LS color
-export LSCOLORS='exfxcxdxbxegedabagacad'
-export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
+export LSCOLORS='gxfxcxdxbxegedabagacad'
+export LS_COLORS='di=36:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 
 # With options
 case $OSTYPE in
