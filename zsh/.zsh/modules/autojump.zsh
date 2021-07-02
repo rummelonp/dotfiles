e2f54fb824df0c306a7e3e69a8fe576da992b376
Kazuya Takeshima <mail@mitukiii.jp>
2021-07-02 19:09:19 +0900

    Check brew command

--- zsh/.zsh/modules/autojump.zsh
+++ zsh/.zsh/modules/autojump.zsh
@@ -4,7 +4,7 @@
 
 if [ -f /usr/share/autojump/autojump.zsh ]; then
     source /usr/share/autojump/autojump.zsh
-elif [[ -f $(brew --prefix)/etc/profile.d/autojump.sh ]]; then
+elif (( $+commands[brew] )) && [ -f $(brew --prefix)/etc/profile.d/autojump.sh ]; then
     source $(brew --prefix)/etc/profile.d/autojump.sh
     # Sort fpath
     fpath=(
