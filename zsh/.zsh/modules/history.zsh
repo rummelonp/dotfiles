f1a14f50fcd53be0e4a39ee30b0c6a4982f889bb
Kazuya Takeshima <mail@mitukiii.jp>
2021-07-02 18:11:00 +0900

    Sort history options

--- zsh/.zsh/modules/history.zsh
+++ zsh/.zsh/modules/history.zsh
@@ -9,14 +9,14 @@ SAVEHIST=100000
 
 ### Options ###
 setopt BANG_HIST               # Treat the '!' character specially during expansion.
-setopt NO_EXTENDED_HISTORY     # Write the history file in the 'command' format.
-setopt HIST_BEEP               # beep when accessing non-existent history.
+setopt SHARE_HISTORY           # Share history between all sessions.
 setopt HIST_EXPIRE_DUPS_FIRST  # Expire a duplicate event first when trimming history.
-setopt HIST_FIND_NO_DUPS       # Do not display a previously found event.
-setopt HIST_IGNORE_ALL_DUPS    # Delete an old recorded event if a new event is a duplicate.
 setopt HIST_IGNORE_DUPS        # Do not record an event that was just recorded again.
+setopt HIST_IGNORE_ALL_DUPS    # Delete an old recorded event if a new event is a duplicate.
+setopt HIST_FIND_NO_DUPS       # Do not display a previously found event.
 setopt HIST_IGNORE_SPACE       # Do not record an event starting with a space.
 setopt HIST_SAVE_NO_DUPS       # Do not write a duplicate event to the history file.
-setopt HIST_VERIFY             # do not execute immediately upon history expansion.
+setopt HIST_VERIFY             # Do not execute immediately upon history expansion.
+setopt HIST_BEEP               # Beep when accessing non-existent history.
 setopt INC_APPEND_HISTORY      # Write to the history file immediately, not when the shell exits.
-setopt SHARE_HISTORY           # share history between all sessions.
+unsetopt EXTENDED_HISTORY      # Write the history file in the 'command' format.
