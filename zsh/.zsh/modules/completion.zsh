6f017895ea670b4076fa0fde347ec77a3de959e1
Kazuya Takeshima <mail@mitukiii.jp>
2021-07-02 18:17:12 +0900

    Sort option names

--- zsh/.zsh/modules/completion.zsh
+++ zsh/.zsh/modules/completion.zsh
@@ -6,13 +6,13 @@ autoload -Uz compinit
 compinit -i
 
 ### Options ###
-setopt ALWAYS_TO_END     # Move cursor to the end of a completed word.
 setopt COMPLETE_IN_WORD  # Complete from both ends of a word.
-setopt AUTO_LIST         # Automatically list choices on ambiguous completion.
+setopt ALWAYS_TO_END     # Move cursor to the end of a completed word.
 setopt AUTO_MENU         # Show completion menu on a successive tab press.
+setopt AUTO_LIST         # Automatically list choices on ambiguous completion.
 setopt AUTO_PARAM_SLASH  # If completed parameter is a directory, add a trailing slash.
-unsetopt FLOW_CONTROL    # Disable start/stop characters in shell editor.
 unsetopt MENU_COMPLETE   # Do not autoselect the first completion entry.
+unsetopt FLOW_CONTROL    # Disable start/stop characters in shell editor.
 
 ### Word separator ###
 autoload -Uz select-word-style
--- zsh/.zsh/modules/environment.zsh
+++ zsh/.zsh/modules/environment.zsh
@@ -24,12 +24,12 @@ unsetopt CLOBBER            # Do not overwrite existing files with > and >>.
 unsetopt MAIL_WARNING       # Don't print a warning message if a mail file has been accessed.
 
 # Job control
-setopt AUTO_RESUME          # Attempt to resume existing job before creating a new process.
 setopt LONG_LIST_JOBS       # List jobs in the long format by default.
+setopt AUTO_RESUME          # Attempt to resume existing job before creating a new process.
 setopt NOTIFY               # Report status of background jobs immediately.
 unsetopt BG_NICE            # Don't run all background jobs at a lower priority.
-unsetopt CHECK_JOBS         # Don't report on jobs when shell exit.
 unsetopt HUP                # Don't kill jobs on shell exit.
+unsetopt CHECK_JOBS         # Don't report on jobs when shell exit.
 
 # Script and functions
 setopt MULTIOS              # Write to multiple descriptors.
