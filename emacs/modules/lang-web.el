33a83b431be918ce64e697685c7533c9aec2b6da
Kazuya Takeshima <mail@mitukiii.jp>
2021-11-09 07:42:49 +0900

    Disable semicolon warning of JS

--- emacs/modules/lang-web.el
+++ emacs/modules/lang-web.el
@@ -44,6 +44,7 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
 (setq js2-basic-offset 2)
+(setq js2-strict-missing-semi-warning nil)
 ;; typescript-mode.el
 (require 'typescript-mode)
 (setq typescript-indent-level 2)
