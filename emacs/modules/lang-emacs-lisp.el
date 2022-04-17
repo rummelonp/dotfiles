8c57d53b179a289fcc504aaa03516c210c35dbd8
Kazuya Takeshima <mail@mitukiii.jp>
2022-04-17 16:50:13 +0900

    emacs: find-variable-or-function-at-point

--- emacs/modules/lang-emacs-lisp.el
+++ emacs/modules/lang-emacs-lisp.el
@@ -8,3 +8,13 @@
           '(lambda ()
              (when (string-match "\\.el$" buffer-file-name)
                (byte-compile-file buffer-file-name))))
+
+(defun find-variable-or-function-at-point ()
+  (interactive)
+  (let ((vsym (variable-at-point))
+        (fsym (function-called-at-point)))
+    (cond ((and vsym (not (equal vsym 0)))
+           (find-variable-other-window vsym))
+          (fsym
+           (find-function-other-window fsym)))))
+(define-key emacs-lisp-mode-map (kbd "C-.") 'find-variable-or-function-at-point)
