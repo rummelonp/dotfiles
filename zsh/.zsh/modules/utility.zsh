71c73cb1f647f1c91d02b847295c8f7dea0fe4ae
Kazuya Takeshima <mail@mitukiii.jp>
2022-04-21 02:53:55 +0900

    zsh: ruby utility functions

--- zsh/.zsh/modules/utility.zsh
+++ zsh/.zsh/modules/utility.zsh
@@ -90,6 +90,37 @@ alias bi='bundle install'
 alias bu='bundle update'
 alias be='bundle exec'
 
+function _ruby_command_bin() {
+    typeset cmd=$1
+    <<EOF
+function ${cmd}() {
+    if [ -f bin/${cmd} ]; then
+        bin/${cmd} "\$@"
+    else
+        \$(rbenv root)/shims/${cmd} "\$@"
+    fi
+}
+EOF
+}
+
+function _ruby_command_bundle() {
+    typeset cmd=$1
+    <<EOF
+function ${cmd}() {
+    if [ -f Gemfile ]; then
+        bundle exec ${cmd} "\$@"
+    else
+        \$(rbenv root)/shims/${cmd} "\$@"
+    fi
+}
+EOF
+}
+
+eval "$(_ruby_command_bin rails)"
+eval "$(_ruby_command_bin rake)"
+eval "$(_ruby_command_bundle rspec)"
+eval "$(_ruby_command_bundle rubocop)"
+
 ### Python ###
 alias saba='python -m http.server > /dev/null 2>&1 &'
 alias shimesaba='pkill -f "python -m http.server"'
