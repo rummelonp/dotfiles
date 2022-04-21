9e72ff93109fe0cb7940e847e257663f8a28f0a9
Kazuya Takeshima <mail@mitukiii.jp>
2022-04-22 03:51:30 +0900

    emacs: using custom-set-variables at early-init.el

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,11 +1,11 @@
 (setq package-enable-at-startup nil)
 
-(setq inhibit-startup-screen t)
-(setq inhibit-startup-message t)
-(setq initial-scratch-message nil)
-
-(menu-bar-mode t)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(line-number-mode t)
-(column-number-mode t)
+(custom-set-variables
+ '(inhibit-startup-screen t)
+ '(inhibit-startup-message t)
+ '(initial-scratch-message nil)
+ '(menu-bar-mode t)
+ '(tool-bar-mode nil)
+ '(scroll-bar-mode nil)
+ '(line-number-mode t)
+ '(column-number-mode t))
