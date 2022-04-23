95bc8c06cf93442f70d12d4941f17911ed817908
Kazuya Takeshima <mail@mitukiii.jp>
2022-04-24 08:04:49 +0900

    emacs: language-emacs-lisp.el

--- emacs/modules/language-emacs-lisp.el
+++ emacs/modules/language-emacs-lisp.el
@@ -2,15 +2,15 @@
 ;; language - Emacs Lisp
 ;;
 
-(with-eval-after-load 'emacs-lisp-mode
-  (defun mtk/find-anything-at-point ()
-    (interactive)
-    (let ((vsym (variable-at-point))
-          (fsym (function-called-at-point)))
-      (cond ((and vsym (not (equal vsym 0)))
-             (find-variable-other-window vsym))
-            (fsym
-             (find-function-other-window fsym)))))
-  (define-key emacs-lisp-mode-map (kbd "C-.") 'mtk/find-anything-at-point)
-  (with-eval-after-load 'flycheck
-    (flycheck-package-setup)))
+(defun mtk/find-anything-at-point ()
+  (interactive)
+  (let ((vsym (variable-at-point))
+        (fsym (function-called-at-point)))
+    (cond ((and vsym (not (equal vsym 0)))
+           (find-variable-other-window vsym))
+          (fsym
+           (find-function-other-window fsym)))))
+(define-key emacs-lisp-mode-map (kbd "C-.") 'mtk/find-anything-at-point)
+
+(with-eval-after-load 'flycheck
+  (flycheck-package-setup))
