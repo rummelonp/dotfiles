1244dd5135803b3dc4965c6b5aff63cc6bed04ce
Kazuya Takeshima <mail@mitukiii.jp>
2022-04-25 20:41:13 +0900

    emacs: indent by tab in makefile-mode

--- emacs/modules/language-misc.el
+++ emacs/modules/language-misc.el
@@ -7,3 +7,7 @@
 (add-to-list 'auto-mode-alist '("\\.aws/credentials$" . conf-mode))
 (add-to-list 'auto-mode-alist '("/TODO$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.[pi]uml$" . plantuml-mode))
+
+(defun mtk/setup-makefile-mode ()
+  (setq-local indent-tabs-mode t))
+(add-hook 'makefile-mode-hook 'mtk/setup-makefile-mode)
