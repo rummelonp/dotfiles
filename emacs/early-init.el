9425b77a4ace50757c9be487e8fa7204a881839e
Kazuya Takeshima <mail@mitukiii.jp>
2022-04-25 19:30:19 +0900

    emacs: '(initial-frame-alist '((width . 160) (height . 40)))

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -4,6 +4,7 @@
  '(inhibit-startup-screen t)
  '(inhibit-startup-message t)
  '(initial-scratch-message nil)
+ '(initial-frame-alist '((width . 160) (height . 40)))
  '(menu-bar-mode t)
  '(tool-bar-mode nil)
  '(scroll-bar-mode nil)
