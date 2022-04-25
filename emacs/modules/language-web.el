c62c5f69206f9a0c73d934465314d2da419c2737
Kazuya Takeshima <mail@mitukiii.jp>
2022-04-26 08:18:08 +0900

    emacs: .jsx -> web-mode

--- emacs/modules/language-web.el
+++ emacs/modules/language-web.el
@@ -5,6 +5,7 @@
 (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.ts$" . typescript-mode))
 (add-to-list 'auto-mode-alist '("\\.tsx$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.vue$" . web-mode))
