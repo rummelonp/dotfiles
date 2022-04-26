3f81beccaa971379f2b19b5709da546269ab8932
Kazuya Takeshima <mail@mitukiii.jp>
2022-04-27 01:42:00 +0900

    emacs: oops

--- emacs/custom.el
+++ emacs/custom.el
@@ -97,9 +97,3 @@
  '(web-mode-part-padding 0)
  '(web-mode-script-padding 0)
  '(web-mode-style-padding 0))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(font-lock-variable-name-face ((t (:foreground "#87d7ff")))))
