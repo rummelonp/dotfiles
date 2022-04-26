85bbb4c441ab1abe15a863787e0c7309981163c2
Kazuya Takeshima <mail@mitukiii.jp>
2022-04-27 01:05:28 +0900

    emacs: orderless-migemo

--- emacs/modules/extension-completion.el
+++ emacs/modules/extension-completion.el
@@ -15,8 +15,33 @@
 (with-eval-after-load 'consult
   (consult-customize
    consult-recent-file consult-project-buffer consult-buffer
-   :preview-key nil)
-  (require 'orderless))
+   :preview-key nil))
+
+;; orderless
+(with-eval-after-load 'orderless
+  ;; https://nyoho.jp/diary/?date=20210615
+  (defun orderless-migemo (component)
+    (let ((pattern (migemo-get-pattern component)))
+      (condition-case nil
+          (progn (string-match-p pattern "") pattern)
+        (invalid-regexp nil))))
+  (orderless-define-completion-style orderless-default-style
+    (orderless-matching-styles '(orderless-literal
+                                 orderless-regexp)))
+  (orderless-define-completion-style orderless-migemo-style
+    (orderless-matching-styles '(orderless-literal
+                                 orderless-regexp
+                                 orderless-migemo)))
+  (setq completion-category-overrides
+        '((command (styles orderless-default-style))
+          (file (styles orderless-migemo-style))
+          (buffer (styles orderless-migemo-style))
+          (symbol (styles orderless-default-style))
+          (consult-location (styles orderless-migemo-style))
+          (consult-multi (styles orderless-migemo-style))
+          (org-roam-node (styles orderless-migemo-style))
+          (unicode-name (styles orderless-migemo-style))
+          (variable (styles orderless-default-style)))))
 
 ;; company
 (with-eval-after-load 'company
