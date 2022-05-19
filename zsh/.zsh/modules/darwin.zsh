1c8e2a4c742bf2dfeaf8d78fbd30b160ea6a2ec9
Kazuya Takeshima <mail@mitukiii.jp>
2022-05-20 03:39:38 +0900

    zsh: ^x^e open-emacs

--- zsh/.zsh/modules/darwin.zsh
+++ zsh/.zsh/modules/darwin.zsh
@@ -11,15 +11,22 @@ function _copy-line-as-kill() {
     zle kill-line
     print -rn $CUTBUFFER | pbcopy
 }
-zle -N _copy-line-as-kill
 
 function _paste-as-yank() {
     pbpaste
 }
+
+function _open-emacs() {
+    echo 'tell application "Emacs" to activate window' | osascript
+}
+
+zle -N _copy-line-as-kill
 zle -N _paste-as-yank
+zle -N _open-emacs
 
 bindkey '^k' _copy-line-as-kill
 bindkey '^y' _paste-as-yank
+bindkey '^x^e' _open-emacs
 
 ### Functions ###
 function man-preview() {
