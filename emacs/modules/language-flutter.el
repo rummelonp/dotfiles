e1540e4c161b55fc3bb324fdefeb53638e1fb328
Kazuya Takeshima <mail@mitukiii.jp>
2022-05-25 22:53:23 +0900

    emacs: dart-mode-map C-x C-m consult-lsp-file-symbols

--- emacs/modules/language-flutter.el
+++ emacs/modules/language-flutter.el
@@ -4,3 +4,8 @@
 
 (add-hook 'dart-mode-hook #'lsp)
 (add-hook 'dart-mode-hook 'flutter-test-mode)
+
+(with-eval-after-load 'dart-mode
+  (with-eval-after-load #'lsp-mode
+    (bind-keys :map dart-mode-map
+               ("C-x C-m" . consult-lsp-file-symbols))))
