1339b71d95c854306f304f62cad6d91089b4102a
Kazuya Takeshima <mail@mitukiii.jp>
2022-05-28 05:39:59 +0900

    emacs: popwin

--- emacs/modules/extension-window.el
+++ emacs/modules/extension-window.el
@@ -99,9 +99,9 @@
 
 ;;; Popwin
 (with-eval-after-load 'popwin
-  (add-to-list 'popwin:special-display-config '("*Backtrace*"))
-  (add-to-list 'popwin:special-display-config '("*Warnings*"))
-  (add-to-list 'popwin:special-display-config '("*Compile-Log*")))
+  (add-to-list 'popwin:special-display-config '("*Backtrace*"   :noselect t))
+  (add-to-list 'popwin:special-display-config '("*Warnings*"    :noselect t))
+  (add-to-list 'popwin:special-display-config '("*Compile-Log*" :noselect t)))
 
 ;;; Diminish
 (defun mtk/setup-diminish ()
