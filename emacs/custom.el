dc5a2065ab4ded4154dfac5047b19f9f5095203a
Kazuya Takeshima <mail@mitukiii.jp>
2022-06-02 02:01:25 +0900

    emacs: (all-the-icons-completion-mode t)

--- emacs/custom.el
+++ emacs/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(all-the-icons-completion-mode t)
  '(all-the-icons-dired-monochrome nil)
  '(all-the-icons-scale-factor 1.0)
  '(anzu-mode-lighter "")
