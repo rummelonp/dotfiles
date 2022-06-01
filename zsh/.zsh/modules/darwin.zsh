0ddd085074bf19981def9620eb764cf414379fe1
Kazuya Takeshima <mail@mitukiii.jp>
2022-06-02 02:52:50 +0900

    zsh: remove pbcopy & pbpaste

--- zsh/.zsh/modules/darwin.zsh
+++ zsh/.zsh/modules/darwin.zsh
@@ -7,25 +7,10 @@ if ! [[ $OSTYPE =~ darwin ]]; then
 fi
 
 ### Key bindings ###
-function _copy-line-as-kill() {
-    zle kill-line
-    print -rn $CUTBUFFER | pbcopy
-}
-
-function _paste-as-yank() {
-    pbpaste
-}
-
 function _open-emacs() {
     echo 'tell application "Emacs" to activate window' | osascript
 }
-
-zle -N _copy-line-as-kill
-zle -N _paste-as-yank
 zle -N _open-emacs
-
-bindkey '^k' _copy-line-as-kill
-bindkey '^y' _paste-as-yank
 bindkey '^x^e' _open-emacs
 
 ### Functions ###
