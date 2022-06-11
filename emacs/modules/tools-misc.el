f617bb8346648e63bf9ba666fa41c481dffde94d
Kazuya Takeshima <mail@mitukiii.jp>
2022-06-11 17:51:31 +0900

    emacs: (add-hook 'dockerfile-mode-hook #'lsp)

--- emacs/modules/tools-misc.el
+++ emacs/modules/tools-misc.el
@@ -10,3 +10,6 @@
 (defun mtk/setup-makefile-mode ()
   (setq-local indent-tabs-mode t))
 (add-hook 'makefile-mode-hook 'mtk/setup-makefile-mode)
+
+;; Dockerfile
+(add-hook 'dockerfile-mode-hook #'lsp)
