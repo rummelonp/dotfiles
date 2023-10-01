2ed62e52afaa63f475c40cdfc3570baeb7acc2be
Kazuya Takeshima <rummelonp@gmail.com>
2023-10-01 21:04:18 +0900

    homebrew & zsh: openssl@3

--- zsh/.zsh/modules/lang-ruby.zsh
+++ zsh/.zsh/modules/lang-ruby.zsh
@@ -2,7 +2,7 @@
 # Ruby
 #
 
-export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@3)"
 
 ### Aliases ###
 alias bi='bundle install'
