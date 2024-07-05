15c8d55ac96668cf717212e34eb182105c39946d
Kazuya Takeshima <rummelonp@gmail.com>
2024-07-06 01:54:54 +0900

    emacs: define function at toplevel

--- emacs/modules/checker-syntax.el
+++ emacs/modules/checker-syntax.el
@@ -3,22 +3,22 @@
 ;;
 
 ;; Flycheck
+(defun rmp/flycheck-mode-line-status-text (&optional status)
+  (let* ((active (powerline-selected-window-active))
+         (text (pcase (or status flycheck-last-status-change)
+                 (`not-checked "😴")
+                 (`no-checker "😎")
+                 (`running "🤔")
+                 (`errored "😭")
+                 (`finished
+                  (let-alist (flycheck-count-errors flycheck-current-errors)
+                    (if (or .error .warning)
+                        (concat (when .error "😰") (when .warning "😥"))
+                      "🤩")))
+                 (`interrupted "🤯")
+                 (`suspicious "🙃"))))
+    (concat " " flycheck-mode-line-prefix text)))
 (with-eval-after-load 'flycheck
-  (defun rmp/flycheck-mode-line-status-text (&optional status)
-    (let* ((active (powerline-selected-window-active))
-           (text (pcase (or status flycheck-last-status-change)
-                   (`not-checked "😴")
-                   (`no-checker "😎")
-                   (`running "🤔")
-                   (`errored "😭")
-                   (`finished
-                    (let-alist (flycheck-count-errors flycheck-current-errors)
-                      (if (or .error .warning)
-                          (concat (when .error "😰") (when .warning "😥"))
-                        "🤩")))
-                   (`interrupted "🤯")
-                   (`suspicious "🙃"))))
-      (concat " " flycheck-mode-line-prefix text)))
   (setq flycheck-mode-line '(:eval (rmp/flycheck-mode-line-status-text))))
 
 ;; Auto Fix
