f6bc5cf294b0278f3127148a4b407f6e48528cc3
Kazuya Takeshima <rummelonp@gmail.com>
2024-10-16 20:54:24 +0900

    emacs: go auto format

--- emacs/modules/prog-go.el
+++ emacs/modules/prog-go.el
@@ -2,4 +2,8 @@
 ;; Prog - Go
 ;;
 
-(add-hook 'go-mode-hook #'lsp)
+(defun rmp/setup-go-mode ()
+  (lsp)
+  (add-hook 'before-save-hook #'lsp-format-buffer t t))
+
+(add-hook 'go-mode-hook 'rmp/setup-go-mode)
