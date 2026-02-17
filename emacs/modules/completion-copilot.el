fa031c92ba46a8e344135c4a5ac6432bd67ae932
Kazuya Takeshima <rummelonp@gmail.com>
2026-02-17 19:34:38 +0900

    emacs: copilot-mode without typescript-mode

--- emacs/modules/completion-copilot.el
+++ emacs/modules/completion-copilot.el
@@ -2,4 +2,7 @@
 ;; Completion - Copilot
 ;;
 
-(add-hook 'prog-mode-hook 'copilot-mode)
+(defun rmp/maybe-copilot-mode ()
+  (unless (memq major-mode '(typescript-mode rmp/web-typescript-mode))
+    (copilot-mode 1)))
+(add-hook 'prog-mode-hook 'rmp/maybe-copilot-mode)
