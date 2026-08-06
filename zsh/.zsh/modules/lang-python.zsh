282fe858d22b8c556873eacad9b5aaeba8c534b9
Kazuya Takeshima <rummelonp@gmail.com>
2026-08-06 18:12:04 +0900

    python: saba

--- zsh/.zsh/modules/lang-python.zsh
+++ zsh/.zsh/modules/lang-python.zsh
@@ -2,5 +2,5 @@
 # Python
 #
 
-alias saba='python -m http.server > /dev/null 2>&1 &'
-alias shimesaba='pkill -f "python -m http.server"'
+alias saba='python -m http.server 8000 > /dev/null 2>&1 &'
+alias shimesaba='pkill -f "python -m http.server 8000"'
